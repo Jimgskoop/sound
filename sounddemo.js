1484ac797e42eba719df8256b6e9fe0aedfb8379
Jim Gourgoutis <jimg@yojimg.net>
2012-01-04 13:45:04 -0800

    adding more files

---
+++ sounddemo.js
@@ -0,0 +1,18 @@
+/* 
+   1. Figure out the note value from each key
+   2. Generate binary sample data for that note
+   3. Dynamically create an audio element for each key	
+   4. Create event listener for each button to trigger audio playback
+*/
+
+$(document).on('ready', function() {
+      
+	$(".keys button").each(function (index, ele) {
+
+	 });
+
+	
+
+});
+
+
