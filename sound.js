a0415c6b9655170725e63fe5a95425f5b6dfe732
Jim Gourgoutis <jgourgoutis@linkedin.com>
2012-01-04 17:53:09 -0800

    got demo working

--- sound.js
+++ sound.js
@@ -9,23 +9,25 @@ var info = {
     channels: 2,
     rate: 44100,
     bits: 16 
-},
+};
 
 
 // Convert a number into a binary string of the specified byte length.
 function encInt(n, bytes) {
-    var cs = "",
-	byte = 0;
-    for (var i = 0; i < bytes; i++) {
-	cs += String.fromCharCode((n >> i*8) & 0xff);
-    }
-    return cs;
+  var cs = "";
+
+  for (var i = 0; i < bytes; i++) {
+    cs += String.fromCharCode((n >> i*8) & 0xff);
+  }
+  return cs;
 }
 
 // Thanks to https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
 function wav(info, data) {
+  
     var wavData = "data:audio/x-wav;base64,",
-	binaryData = "";
+	    binaryData = "";
+	  
     binaryData += "RIFF";                                             // Start of RIFF header
     binaryData += encInt(36 + data.length, 4);                        // Rest of file size
     binaryData += "WAVE";                                             // Format
@@ -35,70 +37,144 @@ function wav(info, data) {
     binaryData += encInt(info.channels, 2);                           // Channels
     binaryData += encInt(info.rate, 4);                               // Sample rate
     binaryData += encInt(info.rate * info.channels * info.bits/8, 4); // Byte rate
-    binaryData += encInt(info.channels * info.bits, 2);               // Block align
+    binaryData += encInt(info.channels * info.bits/8, 2);               // Block align
     binaryData += encInt(info.bits, 2);                               // Bits per sample
     binaryData += "data";                                             // Subchunk 2 id
     binaryData += encInt(data.length, 4);                             // Subchunk size
     binaryData += data;                                               // Audio data
     wavData += btoa(binaryData);
+    
     return wavData;
 }
 
 // Convert an array of 2-dimensional floating point sample arrays into binary wave sample data.
 function wavEncode(info, samples) {
-    var data = "";
-    for (var i = 0; i < samples.length; i++) {
-	var sample = samples[i];
-	for (var j = 0; j < sample.length; j++) {
+  var data = "";
+  for (var i = 0; i < samples.length; i++) {
+    var sample = samples[i];
+	  for (var j = 0; j < sample.length; j++) {
 	    value = sample[j] * (Math.pow(2, info.bits-1) - 1);
 	    data += encInt(Math.round(value), info.bits/8);
-	}
-    }
-    return data;
+	  }
+  }
+  return data;
 }
 
 // Adapted from https://github.com/gasman/jasmid/blob/master/synth.js
 function SineGenerator(info, freq) {
-    var self = {'alive': true},
-	period = info.rate / freq,
-	    t = 0;
+  var self = {'alive': true},
+	  period = info.rate / freq,
+	  t = 0;
 
 	self.read = function() {
-	    var phase = t / period;
-	    var result = Math.sin(phase * 2 * Math.PI);
+	    var phase = t / period,
+	      result = Math.sin(phase * 2 * Math.PI);
 	    t++;
 	    return [result, result];
-	}
+	};
 	return self;
 }
 
+
 // Adapted from https://github.com/gasman/jasmid/blob/master/synth.js
-function AttackDecayGenerator(info, child, attackTimeS, decayTimeS, amplitude) {
-    var self = {'alive': true}
-    var attackTime = info.rate * attackTimeS;
-    var decayTime = info.rate * decayTimeS;
-    var t = 0;
+function AttackDecayGenerator(rate, attackTimeS, decayTimeS, child, amplitude) {
 
-    self.read = function() {
-	if (!self.alive) return [0,0];
-	var input = child.read();
-	self.alive = child.alive;
+  var self = {'alive': true},
+    attackTime = rate * attackTimeS,
+    decayTime = rate * decayTimeS,
+    t = 0;
 
-	if (t < attackTime) {
-	    var localAmplitude = amplitude * (t / attackTime);
-	} else {
-	    var localAmplitude = amplitude * (1 - (t - attackTime) / decayTime);
-	    if (localAmplitude <= 0) {
-		self.alive = false;
-		return [0,0];
-	    }
-	}
+  amplitude = amplitude || 1;
 
-	t++;
-	return [localAmplitude * input[0], localAmplitude * input[1]];
+  self.read = function() {
+    var input,
+      localAmplitude;
+    
+    if (!self.alive) {
+      return [0,0];
     }
-    return self;
+    
+    input = child.read();
+    self.alive = child.alive;
+
+    if (t < attackTime) {
+      localAmplitude = amplitude * (t / attackTime);
+    } else {
+      localAmplitude = amplitude * (1 - (t - attackTime) / decayTime);
+      if (localAmplitude <= 0) {
+        self.alive = false;
+        return [0,0];
+      }
+    }
+
+    t++;
+    return [localAmplitude * input[0], localAmplitude * input[1]];
+  };
+  return self;
+}
+
+// Adapted from https://github.com/gasman/jasmid/blob/master/synth.js
+function XAttackDecayGenerator(info, child, attackTimeS, decayTimeS, amplitude) {
+  var self = {'alive': true},
+    attackTime = info.rate * attackTimeS,
+    decayTime = info.rate * decayTimeS,
+    t = 0;
+
+  self.read = function() {
+    var input,
+      localAmplitude;
+
+    if (!self.alive) return [0,0];
+
+    input = child.read();
+    self.alive = child.alive;
+
+    if (t < attackTime) {
+      localAmplitude = amplitude * (t / attackTime);
+    } else {
+      localAmplitude = amplitude * (1 - (t - attackTime) / decayTime);
+      if (localAmplitude <= 0) {
+        self.alive = false;
+        return [0,0];
+      }
+    }
+
+    t++;
+    return [localAmplitude * input[0], localAmplitude * input[1]];
+  };
+    
+  return self;
 }
 
 
+function synthesize(gen, duration, samples) {
+    samples = samples || [];
+    for (var i = 0; i < duration; i++) {
+        samples.push(gen.read());
+    }
+    return samples;
+}
 
+
+function echo(rate, delay, samples) {
+  var i,
+    j,
+    sample,
+    echoSample,
+    newSample,
+    offset = delay * rate,
+    length = samples.length = samples.length + offset * 8;
+
+  samples = samples.concat();
+
+  for (i = 0; i < length; i++) {
+    sample = samples[i];
+    echoSample = samples[i + offset] || [];
+    newSample = [];
+    for (j = 0; j < sample.length; j++) {
+      newSample[j] = ((echoSample[j] || 0) + sample[j])/2;
+    }
+    samples[i + offset] = newSample;
+  }
+  return samples;
+}
--- sounddemo.js
+++ sounddemo.js
@@ -5,13 +5,68 @@
    4. Create event listener for each button to trigger audio playback
 */
 
-$(document).on('ready', function() {
+
+$(document).ready(function() {
       
-	$(".keys button").each(function (index, ele) {
+  var $keys = $(".keys"),
+    players = {};
+      
+  $("button", $keys).each(function (index, ele) {
+    var $ele = $(ele),
+      note,
+      noteFreq,
+      sine,
+      tone,
+      sample,
+      samples,
+      wavData,
+      wavURI,
+      attack = 0.001,
+      decay = 0.25,
+      sustain = 8000,
+      audioNode;
+    
+    // 1. Figure out the note value from each key
+    note = $ele.html().toUpperCase();
+    noteFreq = notes[note];
 
-	 });
+    
+    // 2. Generate the binary sample data for that note
+    sine = SineGenerator(info, noteFreq);
+    sine = AttackDecayGenerator(info.rate, attack, decay, sine);
+    samples = synthesize(sine, sustain);
+    //samples = echo(8000, 0.75, samples);
+    wavData = wavEncode(info, samples);
+    wavURI = wav(info, wavData);    
+    
+    // 3. Dynamically create an audio element for each key	
+    audioNode = new Audio(wavURI);
+    $ele.data('player', audioNode);    
+    players[$ele.attr('data-yojimg-keycode')] = audioNode;
 
-	
+  });
+
+
+
+  // 4. Create delegated event listener for each button to trigger audio playback
+  $keys.on("click", "button", function(event) {
+    event.preventDefault();
+    $(this).data('player').play();
+  });
+    
+  $('body').on("keydown", function(event) {
+    event.preventDefault();
+    
+    var key = event.which;
+    
+    if (typeof players[key] !== "undefined") {
+      players[key].play();
+    }
+    
+  });
+    
+  
+
 
 });
 
